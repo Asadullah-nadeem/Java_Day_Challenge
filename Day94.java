7cc555023abae2b3f8c4a2e2ca419f5860f5454a
Asadullah <88024587+Asadullah-nadeem@users.noreply.github.com>
2023-05-20 20:37:26 +0530

    update

---
+++ Day94.java
@@ -0,0 +1,10 @@
+public class Day94 {
+    public static void main(String[] args) {
+        for (int x = 5; x>=1; x--){
+            for (int y = 1; y<=5; y++){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+}
